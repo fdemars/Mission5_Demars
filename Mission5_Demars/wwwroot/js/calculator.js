5f69df0d537df8abd35f03a722cb473057c9e950
fieldingdemars <fieldingdemars@FIELDINGDEMEF01>
2024-02-10 00:21:25 -0700

    should be everything that didn't sync

---
+++ Mission5_Demars/wwwroot/js/calculator.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $("#calculateTotal").click(function () {
+        var hours = parseInt($("#hours").val());
+        if (!isNaN(hours) && hours > 0) {
+            var chargePerHour = parseFloat($("#chargePerHour").val().replace('$', ''));
+            var total = hours * chargePerHour;
+            $("#totalCost").text("$" + total.toFixed(2));
+        } else {
+            alert("Please enter a valid number of hours.");
+        }
+    });
+});
